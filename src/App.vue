63c934a0f47788bbb58747c53952f46187c7272c
narcotics726 <narcotics726@gmail.com>
2018-01-24 22:36:35 +0800

    use grid to layout the main page.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="container">
-    <div class="header">
+    <div class="header testbox">
       <!-- 
+        
        -->
+       header
     </div>
-    <div class="left">
+    <div class="left testbox">
       <!-- 
         nav menu here
        -->
+       left
     </div>
-    <div class="main">
+    <div class="main testbox">
       <!--
         investigator sheet here.
       -->
+      main
     </div>
-    <div class="footer">
-
+    <div class="footer testbox">
+footer
     </div>
   </div>
 </template>
@@ -44,7 +48,41 @@ body {
 </style>
 
 <style scoped>
+.container {
+    display: grid;
+}
+@media(max-width: 768px) {
+  .container {
+    grid-template-columns: 80px 2fr;
+  }
+}
 
+@media (min-width: 769px) {
+  .container {
+    grid-template-columns: 200px 2fr;
+  }
+}
+
+.testbox {
+  border: 1px solid;
+  min-height: 50px;
+}
+
+.header {
+  grid-column: span 3;
+}
+
+.left {
+  grid-column: 1;
+}
+
+.main {
+  grid-column: 2 span 2;
+}
+
+.footer {
+  grid-column: span 3;
+}
 </style>
 
 
